326796584f55c8f13b4a12a1f2a7cefc198afafc
yyoungeun <1150amy@naver.com>
2020-10-25 14:07:06 +0900

    1025 ~9.3 commit

---
+++ src/CSSModule.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './CSSModule.module.scss';
+
+const CSSModule = () => {
+    console.log(styles);
+    return (
+        <div className={`${styles.wrapper} ${styles.inverted}`}>
+            안녕하세요, 저는 <span className="something">CSS Module!</span>
+        </div>
+    );
+   
+};
+
+export default CSSModule;
