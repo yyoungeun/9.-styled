2dac9c4b76a973486308daa63a15df01c5c9aa20
yyoungeun <1150amy@naver.com>
2020-10-25 14:50:54 +0900

    1025 commit end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
-import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
+//import CSSModule from './CSSModule';
 //import SassComponent from './SassComponent';
 
 class App extends Component {
   render(){
    return (
      <div>
-             <CSSModule/>
+       <StyledComponent/>
+             {/* <CSSModule/> */}
      </div>
     );
   }
---
+++ src/StyledComponent.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+/* 단순 변수의 형태가 아니라 여러 줄의 스타일 구문을 조건부로 설정해야 하는 경우에는 css를 불러와야 합니다.*/
+
+const Box = styled.div`
+/*  props로 넣어준 값을 직접 전달해 줄 수 있습니다. */
+background: ${props => props.color || 'blue'}; /* 기본값은 blue */
+padding: 1rem;
+display: 'flex';
+/* 기본적으로 가로크기 1024px에 가운데 정렬을 하고
+   가로 크기가 작아짐에 따라 크기를 줄이고
+   768px 미만이 되면 꽉 채웁니다. */
+   width: 1024px;
+   margin: 0 auto;
+   @media (max-width: 1024px){
+       width: 768px;
+   }
+   @media (max-width: 768px){
+       width: 100%;
+   }
+`;
+
+const Button =styled.button`
+background: white;
+color: black;
+border-radius:4px;
+padding: 0.5rem;
+display: flex;
+align-items: center;
+justify-content: center;
+box-sizing: border-box;
+font-size: 1rem;
+font-weight: 600;
+
+/* &문자를 사용하여 Sass처럼 자기 자신 선택가능 */
+&:hover {
+    background: rgba(255, 255, 255, 0.9);
+}
+
+/* 다음 코드는 inverted값이 true일 때 특정 스타일을 부여해 줍니다. */
+${props => props.inverted && 
+    css`
+        background: none;
+        border: 2px solid white;
+        color: white;
+        &:hover {
+            background: white;
+            color: black;
+        }
+    `};
+    & +button {
+        margin-left: 1rem;
+    }
+    `;
+
+    const StyledComponent = () => (
+        <Box color="black">
+            <Button>안녕하세요</Button><hr/>
+            <Button inverted={true}>테두리만</Button>
+        </Box>
+    );
+
+    export default StyledComponent;
